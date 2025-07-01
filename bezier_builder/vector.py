b7e1277ad15c7e8bad38b6858003c2f83bd1fbca
Kelly Egan <kelly.egan@gmail.com>
2025-07-01 09:52:41 -0400

    Create vector class

---
+++ bezier_builder/vector.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class Vector(np.ndarray):
+    def __new__(cls, x=0, y=0):
+        obj = np.array([x, y], dtype=np.float64).view(cls)
+        return obj
+    
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+    
+    @property
+    def x(self) -> float:
+        return self[0]
+    
+    @x.setter
+    def x(self, value: float):
+        self[0] = value
+    
+    @property
+    def y(self) -> float:
+        return self[1]
+    
+    @y.setter
+    def y(self, value: float):
+        self[1] = value
---
+++ tests/test_vector.py
@@ -0,0 +1,33 @@
+import pytest
+import numpy as np
+
+from bezier_builder.vector import Vector
+
+@pytest.fixture
+def vector():
+    return Vector()
+
+
+def test_initialize_defaults(vector):
+    assert vector.shape == (2,)
+    assert vector.dtype == np.float64
+    assert vector.x == 0.0
+    assert vector.y == 0.0
+
+def test_initialize_with_args(vector):
+    vector = Vector(1, 2)
+    assert vector.x == 1.0 
+    assert vector.y == 2.0
+
+def test_setter_methods(vector):
+    vector.x = 3
+    vector.y = 4
+    assert vector[0] == 3.0
+    assert vector[1] == 4.0
+
+def test_add_vectors(vector):
+    vector = Vector(1, 2)
+    other_vector = Vector(5, 6)
+    result = vector + other_vector
+    np.testing.assert_array_equal(result, np.array([6.0, 8.0]))
+    assert isinstance(result, Vector)
